a3a0ed03c3ea322acbe1e85f6a78e456cb6e08df
himanshuparihar\Himanshu <himanshupari007@gmail.com>
2020-07-06 20:16:56 +0530

    H2 database setup

--- src/main/java/org/anb/mii/model/Book.java
+++ src/main/java/org/anb/mii/model/Book.java
@@ -5,18 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 
 @Entity
+@Data
 
 public class Book {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Getter
-	@Setter
+	
 	
 	private Long id;
 	private String name;
