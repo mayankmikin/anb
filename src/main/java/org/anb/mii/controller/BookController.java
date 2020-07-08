113a07a15afa4446787373721046391a8ab5635e
Mayank Verma <mayank.mikin@gmail.com>
2020-07-08 06:47:32 +0200

    ++ create / POST entity database

--- src/main/java/org/anb/mii/controller/BookController.java
+++ src/main/java/org/anb/mii/controller/BookController.java
@@ -1,11 +1,15 @@
 package org.anb.mii.controller;
 
 import org.anb.mii.model.Book;
+import org.anb.mii.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +22,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j	
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BookController {
+	
+	@Autowired
+	private BookRepository bookRepo;
+	
 	@GetMapping("/get")
 	public Book get()
 	{
+		// created object and returned
 		return new Book(new Long(4),"da vinci code","dan brown");
 		//return new ResponseEntity<List<BLETag>> (bleTags, HttpStatus.OK);
 	}
@@ -29,9 +38,20 @@ public class BookController {
 	@GetMapping("/get/{id}")
 	public ResponseEntity<Book> getProdLevel(@PathVariable("id") Long id)
 	{
+		// created object and returned with HTTP status code
 		//return new Book(new Long(4),"da vinci code","dan brown");
 		Book b=new Book(new Long(4),"da vinci code","dan brown");
 		return new ResponseEntity<Book> (b, HttpStatus.ACCEPTED);
 	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<Book> create(@RequestBody Book bookToBeCreated)
+	{
+		log.info("request body :{}",bookToBeCreated);
+		bookToBeCreated=bookRepo.save(bookToBeCreated);
+		log.info("after saving  :{}",bookToBeCreated);
+		return new ResponseEntity<Book> (bookToBeCreated, HttpStatus.ACCEPTED);
+	}
+	
 	
 }
--- src/main/java/org/anb/mii/repository/BookRepository.java
+++ src/main/java/org/anb/mii/repository/BookRepository.java
@@ -2,7 +2,7 @@ package org.anb.mii.repository;
 
 import org.anb.mii.model.Book;
 import org.springframework.data.repository.CrudRepository;
-
+// jpa interacts with DB
 public interface BookRepository extends CrudRepository<Book, Long> {
 
 }
