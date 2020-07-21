7c041469e3495709dcfab00b08e8922e1ccd7a78
Mayank Verma <mayank.mikin@gmail.com>
2020-07-21 15:23:58 +0530

    +++

--- src/main/java/org/anb/mii/repository/BookRepository.java
+++ src/main/java/org/anb/mii/repository/BookRepository.java
@@ -1,13 +1,13 @@
 package org.anb.mii.repository;
 
 import org.anb.mii.model.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 // jpa interacts with DB
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RepositoryRestResource(collectionResourceRel = "book", path = "book")
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
 	//List<Book> findAllById(List<Long> ids);
 	
